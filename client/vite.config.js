b3837c930a5766ddd46359d11f2434aed127c877
Anuja-Patil-15 <anujap700@gmail.com>
2025-07-16 14:56:04 +0530

    Update vite.config.js for build and server settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // optional, default for Vite
+  },
+  server: {
+    host: true, // optional, allows external access (e.g. in LAN)
+    port: 5173, // optional
+  }
 })
